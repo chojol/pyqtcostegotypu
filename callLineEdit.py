720eb25dd62ea30a1cc2e585937aec50cf766e9a
pan jakub <alus@gmail.com>
2023-10-11 12:06:53 +0200

    second commit

--- callLineEdit.py
+++ callLineEdit.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt6.QtWidgets import QDialog
+from PyQt6 import uic
+from PyQt6.QtWidgets import QDialog, QApplication
 
 from layout import *
 
@@ -8,4 +9,18 @@ class Myform(QDialog):
 
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Dialog()
+        #self.ui = Ui_Dialog()
+        #self.ui.setupUi(self)
+        self.ui = uic.loadUi("layout.ui", self)
+        self.ui.ButtonClickMe.clicked.connect(self.dispmessage)
+        self.show()
+
+
+    def dispmessage(self):
+        self.ui.label.setText("hejka pokaz go " + self.ui.lineEditName.text())
+
+if  __name__ =="__main__":
+    app = QApplication(sys.argv)
+    window = Myform()
+    window.show()
+    sys.exit(app.exec())
